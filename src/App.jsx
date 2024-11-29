76546d8509c55d1ccb51ef76d9d61fe374bfcabb
Shem Jaoko <jaokoshem@gmail.com>
2024-11-29 19:53:47 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Hey World</p>
+    </>
+  )
+}
+
+export default App
