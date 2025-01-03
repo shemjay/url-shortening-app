706671962f6553f9774d854b882f8faf2c656522
Shem Jaoko <jaokoshem@gmail.com>
2025-01-03 22:02:20 +0300

    Add responsive navbar menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,59 @@
-import React from 'react'
-import './Navbar.css'
-import Logo from './Assets/Images/logo.svg'
-import Button from './Button'
+import React, { useState } from "react";
+import "./Navbar.css";
+import Logo from "./Assets/Images/logo.svg";
+import Button from "./Button";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className='nav'>
-        <div className='nav__container'>
-            <div className='nav__logo'>
-                <img src={Logo} alt="logo" />
-            </div>
-
-            <ul className='nav__links'>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Resources</a></li>
-                <li><a href="https://cors-anywhere.herokuapp.com/corsdemo" target="blank" rel="noopener noreferrer" >Cors</a></li>
-            </ul>
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="nav__logo">
+          <img src={Logo} alt="logo" />
         </div>
 
-        <div className='nav__buttons'>
-            <button className='nav__buttons nav__login'>Login</button>
+        <div className={`nav__menu ${isOpen ? "active" : ""}`}>
+          <ul className="nav__links">
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+            <li>
+              <a
+                href="https://cors-anywhere.herokuapp.com/corsdemo"
+                target="blank"
+                rel="noopener noreferrer"
+              >
+                Cors
+              </a>
+            </li>
+          </ul>
+          <hr className="nav__menu-underline"></hr>
+          <div className="nav__buttons">
+            <button className="nav__buttons nav__login">Login</button>
             <Button text="Sign Up" />
+          </div>
         </div>
-        
-    </nav>
-  )
-}
 
-export default Navbar
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className={`line ${isOpen ? "open" : ""}`}></div>
+          <div className={`line ${isOpen ? "open" : ""}`}></div>
+          <div className={`line ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
